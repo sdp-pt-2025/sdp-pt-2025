806e8f99f80cd646f02497fd6399ca93f26e395b
Mpumelelo Ntobi <ntobimpumelelo@gmail.com>
2025-09-09 20:38:08 +0200

    bugfix: testing the actual api routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import healthRouter from "./routes/health.js";
+import healthRouter2 from "./src/routes/health.js"
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 // Endpoint handlers
 app.use("/api/health", healthRouter);
+app.use("/api/health2", healthRouter2);
 
 export default app;
