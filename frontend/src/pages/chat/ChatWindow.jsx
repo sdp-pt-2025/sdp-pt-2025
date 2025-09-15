0dbf1bf550bf2119da31a8cf8b90c57a4d7e8842
ClementJele <Njabuloclement06@gmail.com>
2025-09-15 09:27:04 +0200

    BASE

--- frontend/src/pages/chat/ChatWindow.jsx
+++ frontend/src/pages/chat/ChatWindow.jsx
@@ -31,7 +31,7 @@ const ChatWindow = () => {
   
   const fileInputRef = useRef(null);
   const messagesEndRef = useRef(null);
-  const BASE_URL = import.meta.env.VITE_PUBLIC_URL ;
+  const BASE_URL = import.meta.env.BACK_URL ;
   const currentUser = auth.currentUser;
 
   useEffect(() => {
