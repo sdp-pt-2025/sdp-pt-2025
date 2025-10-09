ba6a31353bd4ee0c62972f09cc2e800ce82c8ddf
ClementJele <Njabuloclement06@gmail.com>
2025-10-09 09:56:39 +0200

    api

--- backend/app.js
+++ backend/app.js
@@ -21,9 +21,7 @@ app.use("/api/health2", healthRouter2);
 // Middleware
 app.use(cors({
     origin: process.env.NODE_ENV === "production" 
-        ? ["https://campus-study.vercel.app", 
-          
-        ] 
+        ? ["https://campus-study.vercel.app" ] 
         : ["http://localhost:5173", "http://localhost:5174",], 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
